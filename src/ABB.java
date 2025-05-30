6b42a0a4503c6bcf6f1c97f82a2e88ec77f73260
JeanSilva06 <jeansilva@edu.unifor.br>
2025-05-30 20:44:02 -0300

    :broom: Cleanup: Removendo comentários desnecessários

--- src/ABB.java
+++ src/ABB.java
@@ -5,40 +5,38 @@ public class ABB {
         this.raiz = null;
     }
 
-    // Método para inserir uma palavra na ABB
     public void inserirPalavra(String palavra, int linha) {
         Palavra novaPalavra = new Palavra(palavra);
-        novaPalavra.adicionarOcorrencia(linha); // Adiciona a primeira ocorrência
+        novaPalavra.adicionarOcorrencia(linha);
         
         raiz = inserirRecursivo(raiz, novaPalavra);
     }
 
     private NoABB inserirRecursivo(NoABB no, Palavra palavra) {
         if (no == null) {
-            return new NoABB(palavra); // Cria um novo nó na árvore
+            return new NoABB(palavra);
         }
 
         int comparacao = palavra.getTexto().compareToIgnoreCase(no.getPalavra().getTexto());
         
         if (comparacao < 0) {
-            no.setEsquerda(inserirRecursivo(no.getEsquerda(), palavra)); // Inserir à esquerda
+            no.setEsquerda(inserirRecursivo(no.getEsquerda(), palavra));
         } else if (comparacao > 0) {
-            no.setDireita(inserirRecursivo(no.getDireita(), palavra)); // Inserir à direita
+            no.setDireita(inserirRecursivo(no.getDireita(), palavra));
         } else {
-            no.getPalavra().adicionarOcorrencia(Integer.parseInt(palavra.getOcorrencias().getHead().getPalavra())); // Palavra já existe, adiciona a nova ocorrência
+            no.getPalavra().adicionarOcorrencia(Integer.parseInt(palavra.getOcorrencias().getHead().getPalavra()));
         }
 
         return no;
     }
 
-    // Método para buscar uma palavra na ABB
     public Palavra buscarPalavra(String palavra) {
         return buscarRecursivo(raiz, palavra);
     }
 
     private Palavra buscarRecursivo(NoABB no, String palavra) {
         if (no == null) {
-            return null; // Palavra não encontrada
+            return null;
         }
 
         int comparacao = palavra.compareToIgnoreCase(no.getPalavra().getTexto());
@@ -48,7 +46,7 @@ public class ABB {
         } else if (comparacao > 0) {
             return buscarRecursivo(no.getDireita(), palavra);
         } else {
-            return no.getPalavra(); // Palavra encontrada
+            return no.getPalavra();
         }
     }
 }
